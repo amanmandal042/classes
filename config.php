bae5229124e404abd91c51b7a7d7917caf805162
amanmandal042 <vihapiraha16@gmail.com>
2024-09-19 19:22:51 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', '__-B3');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'cc_Chemcker_roBot');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6490007953');
+
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'cc Chemcker Bot');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'cc_Chemcker_roBot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7264855002:AAHzIFlUTgNi7Ij95tAjNZia_GpGQqnGMZA');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1002335978287');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002414119021');
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'sql8732226');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'sql8.freesqldatabase.com');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'sql8732226');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', '2w3LeB21zv');
